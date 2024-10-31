c50b72f3aeeb8836961b178f4d21d07ee890eb6f
Kai Shen <kai.shen@mail.utoronto.ca>
2024-10-31 16:18:43 -0400

    Added dumpMarkMapInMB parameter.

--- gc/base/GCExtensionsBase.hpp
+++ gc/base/GCExtensionsBase.hpp
@@ -849,6 +849,8 @@ public:
 	bool alwaysCallWriteBarrier; /**< was -Xgc:alwayscallwritebarrier specified? */
 	bool alwaysCallReadBarrier; /**< was -Xgc:alwaysCallReadBarrier specified? */
 
+	int dumpMarkMapInMB; /**< The size of the mark map to dump in MB */
+
 	bool _holdRandomThreadBeforeHandlingWorkUnit; /**< Whether we should randomly hold up a thread entering MM_ParallelTask::handleNextWorkUnit() */
 	uintptr_t _holdRandomThreadBeforeHandlingWorkUnitPeriod; /**< How often (in terms of number of times MM_ParallelTask::handleNextWorkUnit() is called) to randomly hold up a thread entering MM_ParallelTask::handleNextWorkUnit() */
 	bool _forceRandomBackoutsAfterScan; /**< Whether we should force MM_Scavenger::completeScan() to randomly fail due to backout */
@@ -1950,6 +1952,7 @@ public:
 		, _mainThreadCpuTimeNanos(0)
 		, alwaysCallWriteBarrier(false)
 		, alwaysCallReadBarrier(false)
+		, dumpMarkMapInMB(0)
 		, _holdRandomThreadBeforeHandlingWorkUnit(false)
 		, _holdRandomThreadBeforeHandlingWorkUnitPeriod(100)
 		, _forceRandomBackoutsAfterScan(false)
